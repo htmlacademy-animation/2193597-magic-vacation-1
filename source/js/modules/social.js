6012436999dc859cea26180072f75d44fb99debc
Daria <daria7Vo@yandex.ru>
2022-09-24 23:33:33 +0300

    module1 task1

--- source/js/modules/social.js
+++ source/js/modules/social.js
@@ -1,9 +1,14 @@
 export default () => {
   let socialBlock = document.querySelector(`.js-social-block`);
+  let socialBtn = document.querySelector(`.social-block__toggler`);
+  let socialList = document.querySelector(`.social-block__list`);
   socialBlock.addEventListener(`mouseover`, function () {
     socialBlock.classList.add(`social-block--active`);
   });
   socialBlock.addEventListener(`mouseleave`, function () {
     socialBlock.classList.remove(`social-block--active`);
+  });
+  socialBtn.addEventListener(`click`, function () {
+    socialList.classList.toggle(`active`);
   });
 };
